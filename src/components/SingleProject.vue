d4e33a97327200bbdd403431a3918ce5500e4b65
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-05-26 16:52:12 +0300

    class modify

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -29,13 +29,11 @@ export default {
 
 <template>
   <div
-    class="all m-2 relative shadow-md rounded-md p-6 bg-gradient-to-r transform transition duration-500 hover:scale-110"
+    @click="show = !show"
+    class="all cursor-pointer m-2 relative shadow-md rounded-md p-6 bg-gradient-to-r transform transition duration-500 hover:scale-105"
     :class="[project.complete ? complete : todo]"
   >
-    <h1
-      @click="show = !show"
-      class="cursor-pointer text-xl text-white font-bold absolute top-1 left-2"
-    >
+    <h1 class="text-xl text-white font-bold absolute top-1 left-2">
       {{ project.title }}
     </h1>
     <div class="absolute top-2 right-3">
